5c447c17b99cf0fd5a569101d28017b24c9d1d6f
Patriik <61064909+PatriikPlays@users.noreply.github.com>
2023-10-19 22:50:38 +0200

    chore: put db into separate file

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+import { fileURLToPath } from 'node:url';
+import path from "node:path";
 import Fastify from 'fastify'
 import loggerPlugin from "./plugins/logger.js"
 import multipartPlugin from "./plugins/multipart.js";
 import { ShittierError, plugin as errorPlugin } from './plugins/error.js';
 import fastifyCookie from '@fastify/cookie';
 import fs from "node:fs";
-import path from "node:path";
-import { fileURLToPath } from 'node:url';
 import pump from "pump";
-import sqlite3 from 'sqlite3';
 import sanitizeFilename from "sanitize-filename";
 import { generateID } from './utils.js';
 import { DateTime } from "luxon";
+import DB from "./db.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,91 +23,6 @@ await fastify.register(loggerPlugin);
 await fastify.register(errorPlugin);
 await fastify.register(multipartPlugin);
 await fastify.register(fastifyCookie)
-
-class DB {
-    constructor() {
-        this.db = new sqlite3.Database(path.join(__dirname, "data", "db.sqlite"));
-
-        this.db.serialize(() => {
-            this.db.run("CREATE TABLE IF NOT EXISTS links (link_id TEXT, filename TEXT)")
-        })
-    }
-
-    resolveLink(link) {
-        return new Promise((resolve, reject) => {
-            this.db.get("SELECT filename FROM links WHERE link_id = ?", [link], (err, row) => {
-                if (err) {
-                    return reject(err);
-                }
-                if (row) {
-                    resolve(row.filename);
-                } else {
-                    resolve(null);
-                }
-            })
-        })
-    }
-
-    addLink(link, filename) {
-        return new Promise((resolve, reject) => {
-            this.db.run("INSERT INTO links (link_id, filename) VALUES (?, ?)", [link, filename], function(err) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
-        });
-    }
-
-    setLink(link, filename) {
-        return new Promise((resolve, reject) => {
-            this.db.run("UPDATE links SET filename = ? WHERE link_id = ?", [filename, link], function(err) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
-        });
-    }
-    
-    revokeLink(link) {
-        return new Promise((resolve, reject) => {
-            this.db.run("DELETE FROM links WHERE link_id = ?", [link], function(err) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(this.changes > 0);
-                }
-            });
-        });
-    }
-
-    listLinks(filename) {
-        return new Promise((resolve, reject) => {
-            this.db.all("SELECT link_id FROM links WHERE filename = ?", [filename], function(err, rows) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(rows.map(row => row.link_id));
-                }
-            });
-        })
-    }
-
-    deleteLinksPointingToFile(filename) {
-        return new Promise((resolve, reject) => {
-            this.db.run("DELETE FROM links WHERE filename = ?", [filename], function(err) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
-        });
-    }
-}
 
 if (!fs.existsSync("./data")) {
     fs.mkdirSync("./data");
