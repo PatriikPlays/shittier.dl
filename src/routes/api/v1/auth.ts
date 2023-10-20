0542ffa4217eaaadf06e979e3e4a8e71a7c407a5
Patriik <61064909+PatriikPlays@users.noreply.github.com>
2023-10-20 23:30:09 +0200

    feat: add auth verify endpoint

--- src/routes/api/v1/auth.ts
+++ src/routes/api/v1/auth.ts
@@ -21,5 +21,36 @@ export default function(fastify: any, ops: any, done: any) { // TODO: properly d
             }
         }
     })
+
+    fastify.route({
+        method: "POST",
+        url: "/verify",
+        schema: {
+            headers: Type.Object({
+                authorization: Type.String({
+                    minLength: 1,
+                    maxLength: 8192 
+                })
+            })
+        },
+        handler: async (req: any, res: any)=>{
+            let auth = req.headers.authorization.split(" ");
+            if (auth[0] !== "Bearer") return res.send(new ShittierError(403, "Invalid token, Bearer token required"));
+            if (!auth[1]) return res.send(new ShittierError(403, "Invalid token"));
+            
+
+
+            res.send(await new Promise((resolve)=>{
+                try {
+                    fastify.jwt.verify(auth[1], (err: Error, decoded: any)=>{
+                        if (err) resolve(new ShittierError(403, "Invalid token"));
+                        resolve({ ok: true });
+                    })
+                } catch(e) {
+                    if (e) resolve(new ShittierError(403, "Invalid token"));
+                }
+            }))
+        }
+    })
     done()
 }
